c1b6e364d7e3c1fdd6c7ed4e183dc0355e1983f2
Éric BURGHARD <eric@itsufficient.me>
2021-06-03 13:28:26 +0200

    move kakoune utilities to a separate crate

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
 mod error;
-mod escape;
-mod print;
 
-use crate::{
-	error::Error,
-	escape::{EscapeIterator, Token},
-	print::{print_color, print_options},
-};
+use crate::error::Error;
 use std::{env, path::Path, process::Command};
-use yew_ansi::{get_sgr_segments, ColorEffect};
-
+use kak::{face, escape::Mode};
 
 fn main() -> Result<(), Error> {
 	let config_dir = env::var("kak_config")?;
@@ -28,33 +21,7 @@ fn main() -> Result<(), Error> {
 	} else {
 		let stdout = String::from_utf8_lossy(&starship.stdout);
 		if args.first().filter(|v| *v == "prompt").is_some() {
-			for (effect, txt) in get_sgr_segments(&stdout) {
-				let has_option = effect.italic
-					|| effect.underline || effect.bold
-					|| effect.reverse || effect.dim;
-				let has_color = effect.bg != ColorEffect::None || effect.fg != ColorEffect::None;
-				if has_option || has_color {
-					let has_colors =
-						effect.bg != ColorEffect::None && effect.fg != ColorEffect::None;
-					print!("{{");
-					print_color(&effect.fg);
-					if has_colors {
-						print!(",");
-					}
-					print_color(&effect.bg);
-					if has_option {
-						print_options(&effect);
-					}
-					print!("}}");
-				}
-				for token in EscapeIterator::new(txt) {
-					match token {
-						Token::Percent => print!("%%"),
-						Token::Str(txt) => print!("{}", txt),
-						Token::Block(txt) => print!("{}", txt),
-					}
-				}
-			}
+			face::print(&stdout, Mode::Block);
 		} else {
 			println!("{}", stdout);
 			eprintln!("{}", String::from_utf8_lossy(&starship.stderr));
